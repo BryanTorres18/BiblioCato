fcf7ff0733daae3afc3e33ca03b40e884125184e
Deyner24 <deyner.cuadros@ucsm.edu.pe>
2023-11-21 12:51:07 -0500

    Función login gaa

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
+import CryptoJS from "crypto-js";
 
 function Login() {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    Fcode: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = async () => {
+    try {
+      const secretKey = 'PUCHICANAS';
+
+      const encryptedPassword = CryptoJS.AES.encrypt(formData.password, secretKey).toString();
+
+      // Send the plain text password, not the hashed one
+      const dataToSend = { ...formData, password: encryptedPassword };
+  
+      const response = await fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataToSend),
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        console.log(data.message);
+        navigate("/pages/Inicio/Inicio");
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="Login-container">
       <section>
@@ -16,23 +60,37 @@ function Login() {
           <h1>Login</h1>
           <label>
             <h2>Código de alumno/docente:</h2>
-            <input type="text" name="Fcode" />
+            <input
+              type="text"
+              name="Fcode"
+              value={formData.Fcode}
+              onChange={handleChange}
+            />
           </label>
           <label>
             <h2>Contraseña:</h2>
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </label>
           <label className="checkbox-label">
-            <input type="checkbox"/>
+            <input type="checkbox" />
             <span>Recuérdame</span>
           </label>
-          <button className="btm-inicio" button type="button" onClick={() => navigate("/pages/Inicio/Inicio")}>
+          <button
+            className="btm-inicio"
+            type="button"
+            onClick={handleLogin}
+          >
             INGRESAR
           </button>
-          <a href="">¿Olvidastes tu Contraseña?</a>
+          <a href="">¿Olvidaste tu Contraseña?</a>
         </form>
       </section>
-      </div>
+    </div>
   );
 }
 
---
+++ src/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const CryptoJS = require('crypto-js');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'redes',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+  } else {
+    console.log('Conexión exitosa a la base de datos');
+  }
+});
+
+app.post('/login', async (req, res) => {
+    const { Fcode, password } = req.body;
+
+    const secretKey = 'PUCHICANAS';
+  
+    try {
+        const [rows] = await db.promise().query('SELECT * FROM usuario WHERE codUsuario = ?', [Fcode]);
+
+        if (rows.length === 0) {
+        return res.json({ success: false, message: 'Usuario no encontrado' });
+        }
+
+        const user = rows[0];
+
+    const bytes = CryptoJS.AES.decrypt(password, secretKey);
+    const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+    const isPasswordValid = decryptedPassword == user.contrasenia;
+
+    console.log(isPasswordValid);
+
+    if (isPasswordValid) {
+      res.json({ success: true, message: 'Inicio de sesión exitoso' });
+    } else {
+      res.json({ success: false, message: 'Credenciales incorrectas' });
+    }
+    } catch (error) {
+        console.error('Error al realizar la autenticación:', error);
+        res.status(500).send('Error de servidor');
+        }
+  });
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
